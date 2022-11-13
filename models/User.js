0740cc99a7ddb95b95b31a77b922022fae9ea56b
Reece Devenney <reecedevenney@gmail.com>
2022-11-13 18:46:25 -0500

    initial commit

---
+++ models/User.js
@@ -0,0 +1,10 @@
+const { Schema, model } = require('mongoose')
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        require: true,
+        trim: true
+    }
+})
