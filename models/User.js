03633797bb69a27c7cb904678317a75fa3298fac
Reece Devenney <reecedevenney@gmail.com>
2022-11-13 19:20:31 -0500

    added models

--- models/User.js
+++ models/User.js
@@ -4,7 +4,40 @@ const UserSchema = new Schema({
     username: {
         type: String,
         unique: true,
-        require: true,
+        required: true,
         trim: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        match: [/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, 'please enter a valid email']
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
+
+},
+    {
+        toJSON: {
+            virtuals: true
+        }
     }
+)
+
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length
 })
+
+const User = model('User', UserSchema)
+
+module.exports = User
